20638e74732f63a40bbc03cc4e6ea11650b614b6
Andrew Lim <andrewlim@Andrews-MacBook-Air.local>
2026-08-04 23:45:28 -0700

    day 1

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,22 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print("Hello World")
+
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 + 2)
+print(3 - 2)
+print(3 / 2)
+print(2 ** 3)
+print(2 % 3)
+print(2 // 3)
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type("Andrew"))
+print(type([1, 2, 3]))
+print(type({"Name": "Andrew"}))
+print(type({9.8, 3.14, 2.7}))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 30-Days-Of-Python
+
+This is my work and learning process using the 30 days of python git repository.
